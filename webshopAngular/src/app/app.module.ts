0f99238f91da682ddfd148a7f98d7d363ed40c53
Marije Mossel <dm.mossel@gmail.com>
2022-04-01 17:02:01 +0200

    components added

--- webshopAngular/src/app/app.module.ts
+++ webshopAngular/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { ShopComponent } from './shop/shop.component';
+import { ActivitiesComponent } from './activities/activities.component';
+import { AboutComponent } from './about/about.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    ShopComponent,
+    ActivitiesComponent,
+    AboutComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
